5d5eedd56714235d930f71ddc5e06cbad8e3f8f2
Andras Schmelczer <andras@schmelczer.dev>
2024-11-24 15:51:51 +0000

    Reexport funcs

--- backend/reconcile/src/lib.rs
+++ backend/reconcile/src/lib.rs
@@ -1,4 +1,8 @@
 mod diffs;
-pub mod errors;
-pub mod operations;
+mod errors;
+mod operation_transformation;
 mod tokenizer;
+mod utils;
+
+pub use operation_transformation::reconcile;
+pub use operation_transformation::EditedText;
