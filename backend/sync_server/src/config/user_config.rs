af7f6dd8f344d1119ebf6491997377db3eecd76d
Andras Schmelczer <andras@schmelczer.dev>
2024-12-08 17:16:27 +0000

    Add default user

--- backend/sync_server/src/config/user_config.rs
+++ backend/sync_server/src/config/user_config.rs
@@ -1,13 +1,36 @@
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Deserialize, Serialize, Clone, Default)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct UserConfig {
-    #[serde(default = "Vec::new")]
-    pub users: Vec<User>,
+    #[serde(default = "default_users")]
+    pub user_tokens: Vec<User>,
 }
-
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct User {
     pub name: String,
     pub token: String,
+}
+
+impl Default for UserConfig {
+    fn default() -> Self {
+        UserConfig {
+            user_tokens: default_users(),
+        }
+    }
+}
+
+fn default_users() -> Vec<User> {
+    vec![User {
+        name: "admin".to_string(),
+        token: get_random_token(),
+    }]
+}
+
+pub fn get_random_token() -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(64)
+        .map(char::from)
+        .collect()
 }
