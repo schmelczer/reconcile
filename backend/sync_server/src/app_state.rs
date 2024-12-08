53f1a3dcc6bbea4e5b831b392f8df616a4940d88
Andras Schmelczer <andras@schmelczer.dev>
2024-12-08 15:01:50 +0000

    Use canonical config path

--- backend/sync_server/src/app_state.rs
+++ backend/sync_server/src/app_state.rs
@@ -9,9 +9,9 @@ pub struct AppState {
 
 impl AppState {
     pub async fn try_new() -> Result<Self> {
-        let path = std::path::Path::new(CONFIG_PATH);
+        let path = std::path::Path::new(CONFIG_PATH).canonicalize()?;
 
-        let config = Config::read(path).await?;
+        let config = Config::read(&path).await?;
         let database = Database::try_new(&config.database).await?;
 
         Ok(Self { config, database })
