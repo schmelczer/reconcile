64274f4de5c364d107034cfd2c007c146b5b6800
Andras Schmelczer <andras@schmelczer.dev>
2025-01-04 17:04:35 +0000

    Fix splitting logic

--- backend/sync_server/src/utils.rs
+++ backend/sync_server/src/utils.rs
@@ -8,5 +8,26 @@ pub fn sanitize_path(path: &str) -> String {
         replacement: "",
     };
 
-    sanitize_filename::sanitize_with_options(path, options)
+    path.split('/')
+        .map(|part| {
+            let proposal = sanitize_filename::sanitize_with_options(part, options.clone());
+            if !part.is_empty() && proposal.is_empty() {
+                "_".to_owned()
+            } else {
+                proposal
+            }
+        })
+        .collect::<Vec<_>>()
+        .join("/")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_sanitize_path() {
+        assert_eq!(sanitize_path("/my/path/what?"), "/my/path/what");
+        assert_eq!(sanitize_path("/my/path/\\\\:?"), "/my/path/_");
+    }
 }
