39561d386eff7f79309bde39fed06f33fac9ab78
Andras Schmelczer <andras@schmelczer.dev>
2025-02-22 15:13:01 +0000

    Add test-client build files

---
+++ frontend/test-client/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/cli.ts",
+	target: "node",
+	mode: "production",
+	optimization: {
+		minimize: false
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: [".ts", ".js"]
+	},
+	output: {
+		globalObject: "this",
+		filename: "cli.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	plugins: [
+		new webpack.BannerPlugin({ banner: "#!/usr/bin/env node", raw: true })
+	]
+};
