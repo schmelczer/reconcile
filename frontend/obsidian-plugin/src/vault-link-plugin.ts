8a27987798c67649b47aad511ecc362d74558030
Andras Schmelczer <andras@schmelczer.dev>
2025-03-22 17:06:32 +0000

    Make logs view performant

--- frontend/obsidian-plugin/src/vault-link-plugin.ts
+++ frontend/obsidian-plugin/src/vault-link-plugin.ts
@@ -66,7 +66,7 @@ export default class VaultLinkPlugin extends Plugin {
 		);
 		this.registerView(
 			LogsView.TYPE,
-			(leaf) => new LogsView(this, this.client, leaf)
+			(leaf) => new LogsView(this.client, leaf)
 		);
 
 		this.addRibbonIcon(
