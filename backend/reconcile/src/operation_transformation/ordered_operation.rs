df5079efea18a7e6e7a2130266ea16127c85bccc
Andras Schmelczer <andras@schmelczer.dev>
2025-06-14 12:04:19 +0100

    Make OrderedOperation orderable

--- backend/reconcile/src/operation_transformation/ordered_operation.rs
+++ backend/reconcile/src/operation_transformation/ordered_operation.rs
@@ -12,3 +12,37 @@ where
     pub order: usize,
     pub operation: Operation<T>,
 }
+
+impl<T> OrderedOperation<T>
+where
+    T: PartialEq + Clone + std::fmt::Debug,
+{
+    pub fn get_sort_key(&self) -> (usize, usize, String) {
+        (
+            self.order,
+            self.operation.start_index(),
+            // Make sure that the ordering is deterministic regardless of which text
+            // is left or right.
+            match &self.operation {
+                Operation::Equal { index, .. } => index.to_string(),
+                Operation::Insert { text, .. } => text
+                    .iter()
+                    .map(crate::tokenizer::token::Token::original)
+                    .collect::<String>(),
+                Operation::Delete {
+                    deleted_character_count,
+                    ..
+                } => deleted_character_count.to_string(),
+            },
+        )
+    }
+}
+
+impl<T> PartialOrd for OrderedOperation<T>
+where
+    T: PartialEq + Clone + std::fmt::Debug,
+{
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.get_sort_key().partial_cmp(&other.get_sort_key())
+    }
+}
