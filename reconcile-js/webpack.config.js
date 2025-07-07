c38b7c99728f9593aaf52594ad04081913738a0a
Andras Schmelczer <andras@schmelczer.dev>
2025-07-07 22:11:00 +0100

    Have 2 bundles

--- reconcile-js/webpack.config.js
+++ reconcile-js/webpack.config.js
@@ -1,37 +1,55 @@
-const path = require("path");
+const path = require('path');
+const { merge } = require('webpack-merge');
 
-
-module.exports = {
-    entry: "./src/index.ts",
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: ["ts-loader"]
-            },
-            {
-                test: /\.wasm$/,
-                type: "asset/inline"
-            }
-        ]
+const common = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.wasm$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  optimization: {
+    // the consuming project should take care of minification
+    minimize: false,
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      root: __dirname,
+      src: path.resolve(__dirname, 'src'),
     },
-    optimization: {
-        // the consuming project should take care of minification
-        minimize: false
-    },
-    resolve: {
-        extensions: [".ts"],
-        alias: {
-            root: __dirname,
-            src: path.resolve(__dirname, "src")
-        }
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index.js",
-        libraryTarget: "module"
-    },
-    experiments: {
-        outputModule: true
-    },
+  },
+  performance: {
+    hints: false, // it's a library, no need to warn about its size
+  },
 };
+
+module.exports = [
+  merge(common, {
+    target: 'web',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'reconcile.web.js',
+      library: {
+        name: 'reconcile',
+        type: 'umd',
+      },
+      globalObject: 'this',
+    },
+  }),
+  merge(common, {
+    target: 'node',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'reconcile.node.js',
+      libraryTarget: 'commonjs2',
+    },
+  }),
+];
