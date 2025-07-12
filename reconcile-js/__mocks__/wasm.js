6e274350926c8cd6ba11f7370ded546109cf5557
Andras Schmelczer <andras@schmelczer.dev>
2025-07-12 11:35:06 +0100

    Fix tests

--- reconcile-js/__mocks__/wasm.js
+++ reconcile-js/__mocks__/wasm.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Read the actual WASM file and convert to base64 for testing
-const wasmPath = path.join(__dirname, '../../pkg/reconcile_bg.wasm');
+const wasmPath = path.join(__dirname, '../../pkg/reconcile_text_bg.wasm');
 const wasmBuffer = fs.readFileSync(wasmPath);
 const wasmBase64 = wasmBuffer.toString('base64');
 
--- reconcile-js/jest.config.js
+++ reconcile-js/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-babel-esm',
   moduleNameMapper: {
-    '^reconcile-text/reconcile_bg\\.wasm$': `<rootDir>/__mocks__/wasm.js`,
+    '^reconcile-text/reconcile_text_bg\\.wasm$': `<rootDir>/__mocks__/wasm.js`,
   },
 };
