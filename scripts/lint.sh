504872c0adcda87d5a461c0fb0ac1bd3bcca6653
Andras Schmelczer <andras@schmelczer.dev>
2025-12-06 21:30:57 +0000

    Fix lint script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -9,9 +9,11 @@ cargo clippy --all-targets --all-features --fix --allow-dirty --allow-staged
 cargo fmt --all
 
 cd reconcile-js
+npm ci
 npm run format
 
 cd ../examples/website
+npm ci
 npm run format
 
 echo "Success!"
