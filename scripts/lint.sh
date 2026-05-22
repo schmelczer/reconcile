22723cbcae5af3b578ed95c4e952d3a78312c53d
Andras Schmelczer <andras@schmelczer.dev>
2026-05-22 08:07:36 +0100

    Remove

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,9 +5,6 @@ set -e
 which cargo-machete || cargo install cargo-machete
 cargo machete
 
-which lychee || cargo install lychee
-lychee --verbose --exclude npmjs.com README.md
-
 cargo clippy --all-targets --all-features --fix --allow-dirty --allow-staged
 cargo fmt --all
 
